1e6dc8ab4d1d1a644bdcd062dc870b4c4be3fc9b
mjzheng <mjzheng@tencent.com>
2021-04-01 16:05:57 +0800

    update

--- windows/windows_director.py
+++ windows/windows_director.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import record_windows
 import etl_parser
 import setting
@@ -5,41 +6,9 @@ import os
 import flame_graph
 
 
-def export_csv(wpa_exporter, etl_file, profile_file, export_dir):
-    if not os.path.exists(wpa_exporter):
-        print("wpaexporter tool not found")
-        return False
-
-    if not os.path.exists(etl_file):
-        print("etl file not found")
-        return False
-
-    if not os.path.exists(profile_file):
-        print("profile file not found")
-        return False
-
-    print(wpa_exporter)
-    cmd = "\"" + wpa_exporter + "\" -i \"" + etl_file + "\" -profile " + profile_file + \
-          " -outputfolder " + export_dir + " -symbols"
-    print(cmd)
-    try:
-        os.popen(cmd).read()
-    except Exception as e:
-        raise e
-    return True
-
-
 def cap_dump_json():
-    output_dir, etl_file, prefix = record_windows.record_windows_with_config()
-
-    profile_file = os.getcwd() + '\\cpuusage.wpaProfile'
-    wpa_exporter = setting.wpt_dir + "wpaexporter.exe"
-    ret = export_csv(wpa_exporter, etl_file, profile_file, output_dir)
-    if not ret:
-        return None
-
+    output_dir, csv_file, prefix = record_windows.record_export_with_config()
     json_file = prefix + '.json'
-    csv_file = output_dir + "\\CPU_Usage_(Sampled)_Utilization_by_Process,_Thread,_Stack.csv"
     etl_parser.csv_to_json(csv_file, json_file)
 
     flame_file = prefix + "_flame.html"
@@ -51,10 +20,3 @@ def cap_dump_json():
 
 if __name__ == "__main__":
     cap_dump_json()
-    # print('hello')
-    # profile_file = os.getcwd() + '\\cpuusage.wpaProfile'
-    # wpa_exporter = setting.wpt_dir + "wpaexporter.exe"
-    # etl_file = "C:\\Users\\mjzheng\\Documents\\WPR Files\\mjzheng-PC3.03-31-2021.18-43-52.etl"
-    # output_dir = setting.windows_output_dir
-    # ret = export_csv(wpa_exporter, etl_file, profile_file, output_dir)
-    # print('hello')
